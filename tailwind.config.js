11a5f1a90d3ab27518e4955096b70ba5bbc8355e
ElVittor <93359780+ElVittor@users.noreply.github.com>
2024-03-01 17:07:24 -0600

    carga general
    se trabajo co varias cosas

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'], //Tailwind trabaje en los archivos html
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
